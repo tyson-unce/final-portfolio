84d74a0464ecac55d5f94023f7fc59f6f085a84a
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-08-20 21:04:15 -0500

    added a nice contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,88 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 import './styles/Contact.scss';
 
 const Contact = () => {
+	const initialValues = { email: '', feedback: '', name: '' };
+	const [formValues, setFormValues] = useState(initialValues);
+	const [formErrors, setFormErrors] = useState({});
+	const [isSubmit, setIsSubmit] = useState(false);
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormValues({ ...formValues, [name]: value });
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setFormErrors(validate(formValues));
+		setIsSubmit(true);
+	};
+
+	useEffect(() => {
+		console.log(formErrors);
+		if (Object.keys(formErrors).length === 0 && isSubmit) {
+			console.log(formValues);
+		}
+	}, [formErrors]);
+
+	const validate = (values) => {
+		const errors = {};
+		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+		if (!values.email) {
+			errors.email = 'a valid email address is required';
+		} else if (!regex.test(values.email)) {
+			errors.email = 'please input correct email address format';
+		}
+		if (!values.feedback) {
+			errors.feedback = 'a message is required';
+		}
+		if (!values.name) {
+			errors.name = 'please input a name';
+		}
+		return errors;
+	};
 	return (
 		<div className='contact' id='contactpage'>
 			<div className='inputs'>
 				<h1>Contact me</h1>
-				<input type='text' placeholder='Name' />
-				<input type='text' placeholder='Email' />
-				<input type='text' placeholder='Message' className='message' />
-				<button>Send</button>
+				<form className='conForm' onSubmit={handleSubmit}>
+					<input
+						type='name'
+						name='name'
+						placeholder='Name'
+						value={formValues.name}
+						onChange={handleChange}
+					/>
+
+					<input
+						type='email'
+						name='email'
+						placeholder='name@email.com'
+						value={formValues.email}
+						onChange={handleChange}
+					/>
+
+					<input
+						name='feedback'
+						type='text'
+						placeholder='send me a message!'
+						value={formValues.message}
+						onChange={handleChange}
+					/>
+
+					<button type='submit' name='submit' value='Submit'>
+						Send
+					</button>
+					{Object.keys(formErrors).length === 0 && isSubmit ? (
+						<div className='uiMessage'>Thank you for your feedback!</div>
+					) : (
+						''
+					)}
+				</form>
+				<p className='formErrors'>{formErrors.name}</p>
+				<p className='formErrors'>{formErrors.email}</p>
+				<p className='formErrors'>{formErrors.feedback}</p>
 			</div>
 		</div>
 	);
