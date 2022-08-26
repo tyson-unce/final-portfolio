0ed82688bb52f10b703df774df97068503c18de0
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-08-25 20:51:41 -0500

    clean code is good code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import Contact from './components/Contact';
-import Works from './components/Works';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
 	<Router>
-		<Routes>
-			<Route path='/' element={<App />} />
-			<Route path='/contact' element={<Contact />} />
-			<Route path='/works' element={<Works />} />
-			<Route
-				path='/twitter'
-				component={() => {
-					window.location.href = 'https://twitter.com/home';
-					return null;
-				}}
-			/>
-		</Routes>
-	</Router>,
-	document.getElementById('root')
+		<App />
+	</Router>
 );
